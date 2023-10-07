31b1d93087228d0db082a6fdbe435a219441910b
belkin aleksej <belkin.a.a@inbox.ru>
2023-10-07 20:29:53 +0500

    add dotenv

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 import asyncio
 import logging
-
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-import config
 from handlers import router
-
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 
 async def main():
-    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=config.get('BOT_TOKEN'), parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
